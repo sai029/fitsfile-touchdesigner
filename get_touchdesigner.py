7bbf29a6cc3622efe7cd7c3eacb386a8a289f7c2
sai029 <una.zyu1@gmail.com>
2025-06-18 17:56:06 +0900

    バグ修正

--- get_touchdesigner.py
+++ get_touchdesigner.py
@@ -8,11 +8,10 @@ data = [[cell.val for cell in row] for row in op('null1').rows()]
 
 # 正規表現で /data_chunk の後の文字列だけ抽出
 extracted = []
-for row in data:
-    for item in row:
-        match = re.match(r'^/data_chunk "(.*)"$', item)
-        if match:
-            extracted.append(match.group(1))
+for item in data:
+    match = re.match(r'^/data_chunk "(.*)"$', item)
+    if match:
+        extracted.append(match.group(1))
 
 # 抽出した文字列を結合
 json_string = ''.join(extracted)
