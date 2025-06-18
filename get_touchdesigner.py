766906d4397533051526d2872645c04882476d38
NOBU <nobu.453145@gmail.com>
2025-06-18 13:38:10 +0900

    touchdesigner周りの指示追加

--- get_touchdesigner.py
+++ get_touchdesigner.py
@@ -1,17 +1,28 @@
 import numpy as np
 from PIL import Image
+import json
 
 #fitsデータの受け取り
 data = [[cell.val for cell in row] for row in op('null1').rows()]
 
+# データをNumPy配列に変換
+data = np.array(json.loads(data))
 
-# 画像スケーリング（0〜255に正規化）
-data = np.nan_to_num(data)  # NaN除去
-data_min = np.min(data)
-data_max = np.max(data)
-scaled_data = 255 * (data - data_min) / (data_max - data_min)
-scaled_data = scaled_data.astype(np.uint8)
 
-# 画像の表示
-image = Image.fromarray(scaled_data)
-image.show()
+# NaN除去
+data = np.nan_to_num(data)
+
+#データの数だけフレームを表示
+for i in range(len(data[0])):
+    frame = data[0, i, :, :]
+
+    # 画像スケーリング（0〜255に正規化）
+    data_min = np.min(data)
+    data_max = np.max(data)
+    scaled_data = 255 * (data - data_min) / (data_max - data_min)
+    scaled_data = scaled_data.astype(np.uint8)
+
+    # 画像の表示
+    image = Image.fromarray(scaled_data)
+    image.save('/tmp/fits_img.png')
+    op('moviefilein1').par.file = '/tmp/fits_img.png'
